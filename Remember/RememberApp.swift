eb7738778fa98c207c116c0f52082793da7db2a8
asehmb <130265153+asehmb@users.noreply.github.com>
2025-08-19 13:59:25 -0600

    Initial Commit

---
+++ Remember/RememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberApp.swift
+//  Remember
+//
+//  Created by Ajay Sehmbi on 2025-08-19.
+//
+
+import SwiftUI
+
+@main
+struct RememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RememberTests/RememberTests.swift
@@ -0,0 +1,17 @@
+//
+//  RememberTests.swift
+//  RememberTests
+//
+//  Created by Ajay Sehmbi on 2025-08-19.
+//
+
+import Testing
+@testable import Remember
+
+struct RememberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
