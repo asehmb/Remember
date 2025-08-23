21ffc3fb0f7413cbe4d9d656d489f84ad124f822
asehmb <130265153+asehmb@users.noreply.github.com>
2025-08-22 22:40:18 -0600

    added sort sidebar and central searchbar

--- Remember/ContentView.swift
+++ Remember/ContentView.swift
@@ -7,16 +7,56 @@
 
 import SwiftUI
 
+struct Item: Identifiable, Hashable {
+    let name: String
+    let id = UUID()
+}
+
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var columnVisibility = NavigationSplitViewVisibility.all
+    private var sort_by = [
+        Item(name: "Images"),
+        Item(name: "PDFs"),
+        Item(name: "Text"),
+        Item(name: "Links"),
+    ]
+    
+    let items = [
+            "Short", "A bit longer", "Tiny", "Massiveeeee text item",
+            "Medium", "Mini"
+        ]
+
+
+    @State private var multiSelection = Set<Item.ID>()
+    
+    @State private var search_text = ""
+        
+        var body: some View {
+            NavigationSplitView(columnVisibility: $columnVisibility) {
+                Text("Sort")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    .foregroundStyle(.gray)
+                List(sort_by, selection: $multiSelection) { item in
+                    Text(item.name)
+                }
+                .navigationTitle("Sort")
+                .toolbar {
+                }
+                
+                Text("\(multiSelection.count) selections")
+            } detail: {
+                ZStack {
+                    TextField("Search", text: $search_text)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: 200)
+                                        
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+            }
         }
-        .padding()
-    }
 }
 
 #Preview {
